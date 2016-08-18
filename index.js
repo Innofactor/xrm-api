b2aaefc2462b985846b2a89efcca465627576c71
Alexey Shytikov <alexey.shytikov@gmail.com>
2016-08-18 15:51:41 +0200

    Extracting version 0.0.4 from npm: https://www.npmjs.com/package/dynamicscrm-api

--- index.js
+++ index.js
@@ -4,47 +4,45 @@
 var Util    = require('./lib/util.js');
 
 var Dynamics = function (settings) {
-
-    // validates arguments
-    if (!settings || typeof(settings)!=="object") throw new Error("'settings' argument must be a valid object instance.");
+    "use strict";
 
     // creates an instance of class that handles all requests
     var util = new Util(settings);
 
-    this.Authenticate = function(options, cb) {
-        util.Authenticate(options, cb)
+    this.Authenticate = function (options, cb) {
+        util.Authenticate(options, cb);
     };
 
     // 
-    this.Create = function(options, cb) {
+    this.Create = function (options, cb) {
         util.Create(options, cb);
     };
 
-    this.Update = function(options, cb) {
+    this.Update = function (options, cb) {
         util.Update(options, cb);
     };
 
-    this.Retrieve = function(options, cb) {
+    this.Retrieve = function (options, cb) {
         util.Retrieve(options, cb);
     };
 
-    this.RetrieveMultiple = function(options, cb) {
+    this.RetrieveMultiple = function (options, cb) {
         util.RetrieveMultiple(options, cb);
     };
 
-    this.Associate = function(options, cb) {
+    this.Associate = function (options, cb) {
         util.Associate(options, cb);
     };
 
-    this.Disassociate = function(options, cb) {
+    this.Disassociate = function (options, cb) {
         util.Disassociate(options, cb);
     };
 
-    this.Execute = function(options, cb) {
+    this.Execute = function (options, cb) {
         util.Execute(options, cb);
     };
 
-    this.Delete = function(options, cb) {
+    this.Delete = function (options, cb) {
         util.Delete(options, cb);
     };
 };
---
+++ test/index.js
@@ -0,0 +1,244 @@
+/*global describe, before, beforeEach, it */
+
+var assert      = require('assert');
+var Dynamics    = require('../index.js');
+
+var settingsForMicrosoftOnlineAuth = {
+        username : "",
+        password : "",
+        organizationid : "",
+        domain : "",
+        domainUrlSuffix: "",
+        authType: "microsoft_online" //Office365
+    },
+
+    settingsForLiveIdAuth = {
+        username : "",
+        password : "",
+        organizationid : "",
+        domain: "",
+        authType: "live_id"
+    };
+
+describe("Dynamics integration tests.", function () {
+    "use strict";
+
+    describe("Constructor", function () {
+        it("should fail with invalid settings", function () {
+            assert.throws(function () {
+                var dynamics = new Dynamics("");
+                assert.ok(!dynamics);
+            });
+        });
+
+        it("should fail with empty settings", function () {
+            assert.throws(function () {
+                var dynamics = new Dynamics({});
+                assert.ok(!dynamics);
+            });
+        });
+
+        it("should fail with no domain", function () {
+            assert.throws(function () {
+                var dynamics = new Dynamics({organizationid: "foo"});
+                assert.ok(!dynamics);
+            });
+        });
+
+        it("should fail with invalid timeout", function () {
+            assert.throws(function () {
+                var dynamics = new Dynamics({organizationid: "foo",
+                    domain: "foo",
+                    timeout: "bar"});
+                assert.ok(!dynamics);
+            });
+        });
+
+        it("should fail with invalid username", function () {
+            assert.throws(function () {
+                var dynamics = new Dynamics({organizationid: "foo",
+                    domain: "foo",
+                    username: 3600});
+                assert.ok(!dynamics);
+            });
+        });
+
+        it("should fail with invalid password", function () {
+            assert.throws(function () {
+                var dynamics = new Dynamics({organizationid: "foo",
+                    domain: "foo",
+                    password: 3600});
+                assert.ok(!dynamics);
+            });
+        });
+
+        it("should not fail with no authType", function () {
+            var dynamics = new Dynamics({organizationid: "foo",
+                domain: "foo"});
+            assert.ok(dynamics);
+        });
+
+        it("should not fail with invalid type of authType", function () {
+            var dynamics = new Dynamics({organizationid: "foo",
+                domain: "foo",
+                authType: 123});
+            assert.ok(dynamics);
+        });
+
+        it("should not fail with invalid value of authType", function () {
+            var dynamics = new Dynamics({organizationid: "foo",
+                domain: "foo",
+                authType: "invalid"});
+            assert.ok(dynamics);
+        });
+
+        it("should fail with invalid domainUrlSuffix", function () {
+            assert.throws(function () {
+                var dynamics = new Dynamics({organizationid: "foo",
+                    domain: "foo",
+                    authType: "live_id",
+                    domainUrlSuffix: 123});
+                assert.ok(!dynamics);
+            });
+        });
+    });
+
+    describe("LiveID Authentication", function () {
+        var settings,
+            dynamics;
+
+        beforeEach(function () {
+            settings = settingsForLiveIdAuth;
+            dynamics = new Dynamics(settings);
+        });
+
+        it("Should fail with invalid credentials", function (done) {
+            dynamics.Authenticate({username: "invalid"}, function (err, result) {
+                assert.ok(err);
+                assert.ok(!result);
+                done();
+            });
+        });
+
+        it("Should authenticate OK", function (done) {
+            dynamics.Authenticate({}, function (err, result) {
+                assert.ok(!err, err);
+                assert.ok(result);
+                assert.ok(result.KeyIdentifier);
+                assert.ok(result.CiperValue0);
+                assert.ok(result.CiperValue1);
+                done();
+            });
+        });
+    });
+
+    describe("Office 365 Authentication", function () {
+        var settings,
+            dynamics;
+
+        beforeEach(function () {
+            settings = settingsForMicrosoftOnlineAuth;
+            dynamics = new Dynamics(settings);
+        });
+
+        it("Should fail with invalid credentials", function (done) {
+            dynamics.Authenticate({username: "invalid"}, function (err, result) {
+                assert.ok(err);
+                assert.ok(!result);
+                done();
+            });
+        });
+
+        it("Should authenticate OK", function (done) {
+            dynamics.Authenticate({}, function (err, result) {
+                assert.ok(!err);
+                assert.ok(result);
+                assert.ok(result.KeyIdentifier);
+                assert.ok(result.CiperValue0);
+                assert.ok(result.CiperValue1);
+                done();
+            });
+        });
+    });
+
+    var shouldFailDeletionWithInvalidId = function (dynamics, done) {
+        var options = {};
+        options.EntityName = 'lead';
+        options.id = '0f993360-d987-43f7-8995-ab5ffb50a43f';
+
+        dynamics.Authenticate({}, function (err, result) {
+            assert.ok(!err);
+            assert.ok(result);
+
+            dynamics.Delete(options, function (err2, result2) {
+                assert.ok(err2, err2);
+                assert.ok(!result2);
+                assert.equal(err2.message, "Lead With Id = " + options.id + " Does Not Exist");
+                done();
+            });
+        });
+    },
+
+        shouldCreateALeadAndThenDeleteIt = function (dynamics, done) {
+            var options = {};
+            options.LogicalName = 'lead';
+            options.Attributes = [ { key: 'lastname', value : 'Doe'},
+                { key: 'firstname', value : 'John'}];
+
+            dynamics.Authenticate({}, function (err, result) {
+                assert.ok(!err);
+                assert.ok(result);
+
+                dynamics.Create(options, function (err2, result2) {
+                    assert.ok(!err2, err2);
+                    assert.ok(result2);
+
+                    options = {};
+                    options.EntityName = 'lead';
+                    options.id = result2.Envelope.Body.CreateResponse.CreateResult;
+
+                    dynamics.Delete(options, function (err3, result3) {
+                        assert.ok(!err3, err3);
+                        assert.ok(result3);
+                        done();
+                    });
+                });
+            });
+        };
+
+    describe("Method execution with LiveId Auth", function () {
+        var settings,
+            dynamics;
+
+        before(function () {
+            settings = settingsForLiveIdAuth;
+            dynamics = new Dynamics(settings);
+        });
+
+        it("Should fail deletion with invalid id", function (done) {
+            shouldFailDeletionWithInvalidId(dynamics, done);
+        });
+
+        it("Should Create a Lead and then delete it", function (done) {
+            shouldCreateALeadAndThenDeleteIt(dynamics, done);
+        });
+    });
+
+    describe("Method execution with MicrosoftOnline Auth", function () {
+        var settings,
+            dynamics;
+
+        before(function () {
+            settings = settingsForMicrosoftOnlineAuth;
+            dynamics = new Dynamics(settings);
+        });
+
+        it("Should fail deletion with invalid id", function (done) {
+            shouldFailDeletionWithInvalidId(dynamics, done);
+        });
+
+        it("Should Create a Lead and then delete it", function (done) {
+            shouldCreateALeadAndThenDeleteIt(dynamics, done);
+        });
+    });
+});
