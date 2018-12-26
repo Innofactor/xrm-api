59053c44f58702a870120bf0ae125c52d61a5aab
Wilson <wilson.mellado@dattos.com.br>
2018-12-26 13:36:43 -0200

    Fix export and path

--- Main.js
+++ Main.js
@@ -1,40 +1,47 @@
 /*
 * Module's dependencies
 */
-var Util    = require('./src/util.js');
-var Message = require('./src/message.js');
+var Util    = require('./lib/util.js');
+var Message = require('./lib/message.js');
 
-class Dynamics {
-    constructor(settings) {
-        "use strict";
-        // creates an instance of class that handles all requests
-        var util = new Util(settings);
-        var message = new Message(util);
-        this.Create = function (options) {
-            return message.Create(options);
-        };
-        this.Update = function (options) {
-            return message.Update(options);
-        };
-        this.Retrieve = function (options) {
-            return message.Retrieve(options);
-        };
-        this.RetrieveMultiple = function (options) {
-            return message.RetrieveMultiple(options);
-        };
-        this.Associate = function (options) {
-            return message.Associate(options);
-        };
-        this.Disassociate = function (options) {
-            return message.Disassociate(options);
-        };
-        this.Execute = function (options) {
-            return message.Execute(options);
-        };
-        this.Delete = function (options) {
-            return message.Delete(options);
-        };
-    }
-}
+var Dynamics = function (settings) {
+    "use strict";
 
-export default Dynamics;
+    // creates an instance of class that handles all requests
+    var util = new Util(settings);
+    var message = new Message(util);
+    
+    this.Create = function (options) {
+        return message.Create(options);
+    };
+
+    this.Update = function (options) {
+        return message.Update(options);
+    };
+
+    this.Retrieve = function (options) {
+        return message.Retrieve(options);
+    };
+
+    this.RetrieveMultiple = function (options) {
+        return message.RetrieveMultiple(options);
+    };
+
+    this.Associate = function (options) {
+        return message.Associate(options);
+    };
+
+    this.Disassociate = function (options) {
+        return message.Disassociate(options);
+    };
+
+    this.Execute = function (options) {
+        return message.Execute(options);
+    };
+
+    this.Delete = function (options) {
+        return message.Delete(options);
+    };
+};
+
+module.exports = Dynamics;
