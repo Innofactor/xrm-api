a522b0ef38294542b61477fa07577434b5646ce3
kiko <wfmellado@gmail.com>
2018-12-27 18:57:49 -0200

    Fixed queryexpression

--- src/serializer.js
+++ src/serializer.js
@@ -1,5 +1,5 @@
-var Serializer = function () {
-    /*
+var Serializer = function() {
+  /*
     result.EntityName = 'leadsrelation';
     result.EntityId = 'e21a10ec-8209-e111-8660-00155d31e39f';
     result.RelationShip = { PrimaryEntityRole : 'Referencing', SchemaName: 'connectionroleassociation_association'};
@@ -11,34 +11,136 @@ var Serializer = function () {
         }
     ];
     */
-    this.toXmlAssociate= function(options) { 
-        var xml = '' ;
 
-        if (options.EntityName) {
-            xml += "<entityName>"+ options.EntityName +"</entityName>";
-        };
+  this.toXmlRetrieveMultiple = function(options) {
+    var xml = "";
 
-        if (options.EntityId) {
-            xml += "<entityId>"+ options.EntityId +"</entityId>";
-        };
+    if (options.id) {
+      xml += "<b:id>" + options.id + "</b:id>";
+    }
 
-        if (options.RelationShip) {
-            if (options.RelationShip.SchemaName) //options.RelationShip.PrimaryEntityRole && //<b:PrimaryEntityRole>"+ options.RelationShip.PrimaryEntityRole + "</b:PrimaryEntityRole>
-                {
-                    xml += "<relationship><b:SchemaName>" + options.RelationShip.SchemaName +"</b:SchemaName></relationship>";
-                }
-        };
+    if (options.ColumnSet) {
+      var columset = options.ColumnSet.map(function(c) {
+        return "<c:string>" + c + "</c:string>";
+      });
 
-        if (options.RelatedEntities) {
-            var atts = options.RelatedEntities.map(function(c) {
-                return '<b:EntityReference><b:Id>'+ c.Id + '</b:Id><b:LogicalName>'+ c.LogicalName +'</b:LogicalName><b:Name>' + c.Name + '</b:Name></b:EntityReference>';
-            });
-            xml += "<relatedEntities>" + atts.join('') + "</relatedEntities>";
-        };
+      xml +=
+        '<b:ColumnSet>' +
+        '<b:AllColumns>false</b:AllColumns>' +
+        '<b:Columns xmlns:c="http://schemas.microsoft.com/2003/10/Serialization/Arrays">' +
+        columset.join("") +
+        '</b:Columns>' +
+        '</b:ColumnSet>';
+    } else {
+      xml +=
+        '<b:ColumnSet>' +
+        '<b:AllColumns>true</b:AllColumns>' +
+        '<b:Columns xmlns:c="http://schemas.microsoft.com/2003/10/Serialization/Arrays"></b:Columns>' +
+        '</b:ColumnSet>';
+    }
 
-        return xml;
-    }    
-    /*
+    if (options.Criteria) {
+      if (options.Criteria.Conditions) {
+        const conditions = options.Criteria.Conditions.map(c => {
+          return `\n<b:ConditionExpression>
+                       <b:AttributeName>${c.AttributeName}</b:AttributeName>
+                       <b:Operator>${c.Operator}</b:Operator>
+                       <b:Values xmlns:c="http://schemas.microsoft.com/2003/10/Serialization/Arrays">                        
+                        <c:anyType i:type="d:string" xmlns:d="http://www.w3.org/2001/XMLSchema">${c.Value}</c:anyType>
+                       </b:Values>
+                     </b:ConditionExpression>`;
+        });
+
+        const xmlConditions = `\n<b:Conditions>
+                                       ${conditions.join("")}
+                                   </b:Conditions>`;
+
+        if (options.Criteria.FilterOperators) {
+          const filters = options.Criteria.FilterOperators.map(c => {
+            return `<b:FilterOperator>${c}</b:FilterOperator>`;
+          });
+          xml += `\n<b:Criteria>                         
+                    ${xmlConditions}   
+                    ${filters.join("")}
+                    <a:Filters />   
+                    <a:IsQuickFindFilter>false</a:IsQuickFindFilter>                   
+                   </b:Criteria>`;
+        }
+      }
+    }
+
+    if (options.EntityName) {
+      xml += "<b:EntityName>" + options.EntityName + "</b:EntityName>";
+    }
+
+    xml += "<b:Distinct>false</b:Distinct><b:LinkEntities /><b:Orders />";
+
+    if (options.TopCount) {
+      xml += "<b:TopCount>" + options.TopCount + "</b:TopCount>";
+    }
+
+    var pageInfo = options.PageInfo || {};
+    pageInfo.Count = pageInfo.Count || 0;
+    pageInfo.PageNumber = pageInfo.PageNumber || 0;
+    pageInfo.PagingCookie = pageInfo.PagingCookie || null;
+
+    xml +=
+      "<b:PageInfo><b:Count>" +
+      pageInfo.Count +
+      "</b:Count><b:PageNumber>" +
+      pageInfo.PageNumber +
+      "</b:PageNumber>";
+
+    if (pageInfo.PagingCookie === null) {
+      xml += "<b:PagingCookie i:nil='true'/>";
+    } else {
+      xml += "<b:PagingCookie>" + pageInfo.PagingCookie + "</b:PagingCookie>";
+    }
+
+    xml +=
+      "<b:ReturnTotalRecordCount>true</b:ReturnTotalRecordCount></b:PageInfo>";
+    return xml;
+  };
+
+  this.toXmlAssociate = function(options) {
+    var xml = "";
+
+    if (options.EntityName) {
+      xml += "<entityName>" + options.EntityName + "</entityName>";
+    }
+
+    if (options.EntityId) {
+      xml += "<entityId>" + options.EntityId + "</entityId>";
+    }
+
+    if (options.RelationShip) {
+      if (options.RelationShip.SchemaName) {
+        //options.RelationShip.PrimaryEntityRole && //<b:PrimaryEntityRole>"+ options.RelationShip.PrimaryEntityRole + "</b:PrimaryEntityRole>
+        xml +=
+          "<relationship><b:SchemaName>" +
+          options.RelationShip.SchemaName +
+          "</b:SchemaName></relationship>";
+      }
+    }
+
+    if (options.RelatedEntities) {
+      var atts = options.RelatedEntities.map(function(c) {
+        return (
+          "<b:EntityReference><b:Id>" +
+          c.Id +
+          "</b:Id><b:LogicalName>" +
+          c.LogicalName +
+          "</b:LogicalName><b:Name>" +
+          c.Name +
+          "</b:Name></b:EntityReference>"
+        );
+      });
+      xml += "<relatedEntities>" + atts.join("") + "</relatedEntities>";
+    }
+
+    return xml;
+  };
+  /*
     {
         LogicalName : "?",
         Attributes : [ 
@@ -53,54 +155,62 @@ var Serializer = function () {
             } ]
     }
     */
-    this.toXmlCreateUpdate= function(options)
-    {
-        var xml = '' ;
-        if (options.Attributes) {
-            var atts = options.Attributes.map(function(c) {
-                return '<b:KeyValuePairOfstringanyType><c:key>'+ c.key + '</c:key><c:value  i:type="d:string" xmlns:d="http://www.w3.org/2001/XMLSchema">'+ c.value +'</c:value></b:KeyValuePairOfstringanyType>';
+  this.toXmlCreateUpdate = function(options) {
+    var xml = "";
+    if (options.Attributes) {
+      var atts = options.Attributes.map(function(c) {
+        return (
+          "<b:KeyValuePairOfstringanyType><c:key>" +
+          c.key +
+          '</c:key><c:value  i:type="d:string" xmlns:d="http://www.w3.org/2001/XMLSchema">' +
+          c.value +
+          "</c:value></b:KeyValuePairOfstringanyType>"
+        );
+      });
+      xml += "<b:Attributes>" + atts.join("") + "</b:Attributes>";
+    }
+    if (options.id) {
+      xml += "<b:Id>" + options.id + "</b:Id>";
+    }
 
-            });
-            xml += "<b:Attributes>" + atts.join('') + "</b:Attributes>";
-        };
-        if (options.id) {
-            xml += "<b:Id>"+ options.id +"</b:Id>";
-        };
+    if (options.FormatedValues) {
+      var atts = options.FormatedValues.map(function(c) {
+        return (
+          "<c:key>" +
+          c.key +
+          '</c:key><c:value  i:type="d:string" xmlns:d="http://www.w3.org/2001/XMLSchema">' +
+          c.value +
+          "</c:value>"
+        );
+      });
+      xml += "<b:FormattedValues>" + atts.join("") + "</b:FormattedValues>";
+    }
 
-        if (options.FormatedValues) {
-            var atts = options.FormatedValues.map(function(c) {
-                return '<c:key>'+ c.key + '</c:key><c:value  i:type="d:string" xmlns:d="http://www.w3.org/2001/XMLSchema">'+ c.value +'</c:value>';
+    if (options.LogicalName) {
+      xml += "<b:LogicalName>" + options.LogicalName + "</b:LogicalName>";
+    }
+    return xml;
+  };
 
-            });
-            xml += "<b:FormattedValues>" + atts.join('') + "</b:FormattedValues>";
-        };
-
-        if (options.LogicalName) {
-            xml += "<b:LogicalName>"+ options.LogicalName +"</b:LogicalName>";
-        };
-        return xml;
-    };
-
-    /*
+  /*
     {
        EntityName : "?",
        Id : "guid"
     }
     */
-    this.toXmlDelete= function(options)
-    {
-        var xml='' ;
-        if (options.EntityName) {
-            xml += "<entityName>"+ options.EntityName +"</entityName>";
-        };
+  this.toXmlDelete = function(options) {
+    var xml = "";
+    if (options.EntityName) {
+      xml += "<entityName>" + options.EntityName + "</entityName>";
+    }
 
-        if (options.id) {
-            xml += "<id>"+ options.id +"</id>";
-        };    
-        return xml;
-    };
+    if (options.id) {
+      xml += "<id>" + options.id + "</id>";
+    }
+    return xml;
+  };
 
-    /*
+  /*
     {
         RequestName : "?",
         RequestId : "guid",
@@ -111,29 +221,34 @@ var Serializer = function () {
             } ]
     }
     */
-    this.toXmlExecute= function(options)
-    {
-        var xml='' ;
-        
-        if (options.RequestName) {
-            xml = "<b:RequestName>"+ options.RequestName +"</b:RequestName>";
-        };
+  this.toXmlExecute = function(options) {
+    var xml = "";
 
-        if (options.RequestId) {
-            xml += "<b:RequestId>"+ options.RequestId +"</b:RequestId>";
-        };
+    if (options.RequestName) {
+      xml = "<b:RequestName>" + options.RequestName + "</b:RequestName>";
+    }
 
-        if (options.Parameters) {
-            var atts = options.Parameters.map(function(c) {
-                return '<b:KeyValuePairOfstringanyType><c:key>'+ c.key + '</c:key><c:value  i:type="d:string" xmlns:d="http://www.w3.org/2001/XMLSchema">'+ c.value +'</c:value></b:KeyValuePairOfstringanyType>';
-            });
-            xml += "<b:Parameters>" + atts.join('') + "</b:Parameters>";
-        };
+    if (options.RequestId) {
+      xml += "<b:RequestId>" + options.RequestId + "</b:RequestId>";
+    }
 
-        return xml;
-    };
+    if (options.Parameters) {
+      var atts = options.Parameters.map(function(c) {
+        return (
+          "<b:KeyValuePairOfstringanyType><c:key>" +
+          c.key +
+          '</c:key><c:value  i:type="d:string" xmlns:d="http://www.w3.org/2001/XMLSchema">' +
+          c.value +
+          "</c:value></b:KeyValuePairOfstringanyType>"
+        );
+      });
+      xml += "<b:Parameters>" + atts.join("") + "</b:Parameters>";
+    }
 
-    /* Para asociar y desasociar
+    return xml;
+  };
+
+  /* Para asociar y desasociar
     {
         EntityName: "?",
         EntityId: "guid",
@@ -150,126 +265,62 @@ var Serializer = function () {
             } ]
     }
     */
-    this.toXmlAsociation= function(options)
-    {
-        var xml = '';
-        
-        if (options.EntityName) {
-            xml += "<b:entityName>"+ options.EntityName +"</b:entityName>";
-        };
+  this.toXmlAsociation = function(options) {
+    var xml = "";
 
-        if (options.EntityId) {
-            xml += "<b:entityId>"+ options.EntityId +"</b:entityId>";
-        };
-        if (options.Relationship.SchemaName) {
-            xml += "<relationship><b:SchemaName>"+ options.Relationship.SchemaName +"</b:SchemaName></relationship>";
-        };
-        if (options.RelatedEntities) {
-            var atts = options.RelatedEntities.map(function(c) { 
-                return '<b:EntityReference><b:Id>'+ c.Id + '</b:Id><b:LogicalName>'+ c.LogicalName +'</b:LogicalName><b:Name>' + c.Name + '</b:Name></b:EntityReference>';
-            });
-            xml += "<relatedEntities>" + atts.join('') + "</relatedEntities>";
-        };
+    if (options.EntityName) {
+      xml += "<b:entityName>" + options.EntityName + "</b:entityName>";
+    }
 
-        return xml;
-    };
+    if (options.EntityId) {
+      xml += "<b:entityId>" + options.EntityId + "</b:entityId>";
+    }
+    if (options.Relationship.SchemaName) {
+      xml +=
+        "<relationship><b:SchemaName>" +
+        options.Relationship.SchemaName +
+        "</b:SchemaName></relationship>";
+    }
+    if (options.RelatedEntities) {
+      var atts = options.RelatedEntities.map(function(c) {
+        return (
+          "<b:EntityReference><b:Id>" +
+          c.Id +
+          "</b:Id><b:LogicalName>" +
+          c.LogicalName +
+          "</b:LogicalName><b:Name>" +
+          c.Name +
+          "</b:Name></b:EntityReference>"
+        );
+      });
+      xml += "<relatedEntities>" + atts.join("") + "</relatedEntities>";
+    }
 
-    this.toXmlRetrieveMultiple= function(options)
-    {
-        var xml = "";
+    return xml;
+  };
 
-        if (options.id) {
-            xml += "<b:id>" + options.id + "</b:id>";
-        };
+  this.toXmlRetrieve = function(options) {
+    var xml;
 
-        if (options.ColumnSet) {
-            var columset = options.ColumnSet.map(function (c) {
-                return "<c:string>" + c + "</c:string>"
-            });
+    if (options.EntityName) {
+      xml = "<entityName>" + options.EntityName + "</entityName>";
+    }
 
-            xml += "<b:ColumnSet><b:AllColumns>false</b:AllColumns><b:Columns xmlns:c=\"http://schemas.microsoft.com/2003/10/Serialization/Arrays\">" + columset.join('') + "</b:Columns></b:ColumnSet>";
-        };
+    if (options.id) {
+      xml += "<id>" + options.id + "</id>";
+    }
 
-        if (options.Criteria) {
-          if (options.Criteria.Conditions) {
-  
-            const conditions = options.Criteria.Conditions
-            .map((c) => {
-              return `<b:ConditionExpression>
-                        <b:AttributeName>${c.AttributeName}</b:AttributeName>
-                        <b:Operator>${c.Operator}</b:Operator>
-                        <b:Values xmlns:c="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
-                          <c:anyType xmlns:d="http://www.w3.org/2001/XMLSchema" i:type="d:string">
-                            ${c.Value}
-                          </c:anyType>
-                        </b:Values>
-                      </b:ConditionExpression>`;
-            });
-  
-            const xmlConditions = `\n<b:Conditions>
-                                        ${conditions.join('')}
-                                    </b:Conditions>`;
-  
-            if (options.Criteria.FilterOperators) {
-              const filters = options.Criteria.FilterOperators
-              .map((c) => {
-                return `<b:FilterOperator>${c}</b:FilterOperator>`;
-              });
-              xml += `\n<b:Criteria>
-                          ${xmlConditions}
-                          ${filters.join('')}
-                        </b:Criteria>`;            
-            }
-          }
-        }   
-
-        if (options.EntityName) {
-            xml += "<b:EntityName>" + options.EntityName + "</b:EntityName>";
-        };
-
-        xml += "<b:Distinct>false</b:Distinct><b:LinkEntities /><b:Orders />"
-
-        if (options.TopCount) {
-            xml += "<b:TopCount>" + options.TopCount + "</b:TopCount>";
-        };
-
-        var pageInfo = options.PageInfo || {};
-        pageInfo.Count = pageInfo.Count || 0;
-        pageInfo.PageNumber = pageInfo.PageNumber || 0;
-        pageInfo.PagingCookie = pageInfo.PagingCookie || null;
-
-        xml += "<b:PageInfo><b:Count>" +
-            pageInfo.Count + "</b:Count><b:PageNumber>" +
-            pageInfo.PageNumber + "</b:PageNumber>"
-
-        if (pageInfo.PagingCookie === null) {
-            xml += "<b:PagingCookie i:nil='true'/>";
-        } else {
-            xml += "<b:PagingCookie>" + pageInfo.PagingCookie+ "</b:PagingCookie>";
-        }
-        
-        xml += "<b:ReturnTotalRecordCount>false</b:ReturnTotalRecordCount></b:PageInfo>";
-        return xml;
-    };
-
-    this.toXmlRetrieve= function(options)
-    {
-        var xml ;
-
-        if (options.EntityName) {
-            xml = "<entityName>"+ options.EntityName +"</entityName>";
-        };
-
-        if (options.id) {
-            xml += "<id>"+ options.id +"</id>";
-        };
-
-        if (options.ColumnSet) {
-            var columset = options.ColumnSet.map(function(c){return "<c:string>"+c+"</c:string>"});
-            xml += "<columnSet><b:AllColumns>false</b:AllColumns><b:Columns>" + columset.join('') + "</b:Columns></columnSet>";
-        };
-        return xml;
-    };
+    if (options.ColumnSet) {
+      var columset = options.ColumnSet.map(function(c) {
+        return "<c:string>" + c + "</c:string>";
+      });
+      xml +=
+        "<columnSet><b:AllColumns>false</b:AllColumns><b:Columns>" +
+        columset.join("") +
+        "</b:Columns></columnSet>";
+    }
+    return xml;
+  };
 };
 
 module.exports = Serializer;
